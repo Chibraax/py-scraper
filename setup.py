6410e6e07e91bc52e7ab228cc791763ace47fa2a
LeChibre <56515202+Chibraax@users.noreply.github.com>
2020-12-20 17:38:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
     author_email="?",
     description="Proxy scraper using requests and bs4.\n Scrap HTTP/HTTPS/SOCKS proxy.\n Each proxy is stored with his country.",
     long_description=open('README.md').read(),
-    install_requires= ["requests","bs4"]
+    install_requires= ["requests","bs4"],
     url='https://github.com/Chibraax/',
 )
